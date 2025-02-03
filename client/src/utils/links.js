c188e0a82687327f2f8caaa476bc64bb4a785fb7
hr-shuvo <h.r.shuvo9@gmail.com>
2025-02-04 05:25:57 +0600

    navbar

---
+++ client/src/utils/links.js
@@ -0,0 +1,36 @@
+import {FaWpforms} from "react-icons/fa";
+import {MdAdminPanelSettings, MdQueryStats} from "react-icons/md";
+import {ImProfile} from "react-icons/im";
+
+
+const links = [
+    {
+        text: 'add job',
+        path: '.',
+        icon: <FaWpforms/>
+    },
+    {
+        text: 'all jobs',
+        path: 'all-jobs',
+        icon: <MdQueryStats/>
+    },
+    {
+        text: 'stats',
+        path: 'stats',
+        icon: <FaWpforms/>
+    },
+    {
+        text: 'profile',
+        path: 'profile',
+        icon: <ImProfile/>
+    },
+    {
+        text: 'admin',
+        path: 'admin',
+        icon: <MdAdminPanelSettings/>
+    }
+
+];
+
+
+export default  links;
