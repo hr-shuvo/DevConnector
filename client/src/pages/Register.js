af3be1f9324fedf9a9d612d08969c1fa0930325e
hr-shuvo <h.r.shuvo9@gmail.com>
2025-02-04 03:20:38 +0600

    form row component

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,12 +1,27 @@
 import {Link} from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import {FormRow, Logo} from "../components";
 
 const Register = () => {
     return (
-        <div>
-            <h1>Register Page</h1>
+        <Wrapper>
+            <form className='form'>
+                <Logo />
+                <h4>Sign Up</h4>
 
-            <Link to={'/login'}>Login</Link>
-        </div>
+                <FormRow type='text' name='name' placeholder='first name' required={true}/>
+                <FormRow type='text' name='last Name' placeholder='last name'/>
+                <FormRow type='text' name='location' placeholder='earth'/>
+                <FormRow type='text' name='email' placeholder='email@example.com'/>
+                <FormRow type='password' name='password' placeholder='******'/>
+
+                <button type='submit' className='btn btn-block'>submit</button>
+                <p>Already have an account? <Link to='/login'>Login</Link></p>
+
+
+
+            </form>
+        </Wrapper>
     )
 
 }
