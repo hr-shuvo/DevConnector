d6ab2c0c68369fe521806729aad5a08fae16aeb0
hr-shuvo <h.r.shuvo9@gmail.com>
2025-02-03 23:11:20 +0600

    init

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <h1>Home</h1>
+    },
+    {
+        path: '/about',
+        element: <h1>About</h1>
+    }
+])
+
+const App = () => {
+    return     <RouterProvider router={router}/>
+}
+
+
+export default App;
