66d9c4c71ffcbf5ba551148e0513fce522f5311a
hr-shuvo <h.r.shuvo9@gmail.com>
2025-02-08 04:20:01 +0600

    add stats api

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,7 +1,7 @@
 import Job from '../models/JobModel.js';
-import {StatusCodes} from 'http-status-codes';
-import {NotFoundError} from "../errors/customError.js";
-
+import { StatusCodes } from 'http-status-codes';
+import mongoose from "mongoose";
+import day from "dayjs";
 
 
 export const getAllJobs = async (req, res) => {
@@ -55,5 +55,71 @@ export const deleteJob = async (req, res) => {
 
     res.status(StatusCodes.OK).json({msg: 'job removed'});
 }
+
+
+// Stats //////////
+
+export const showStats = async (req, res) => {
+
+    let stats = await Job.aggregate([
+        {$match: {createdBy: new mongoose.Types.ObjectId(req.user.userId)}},
+        {$group: {_id: '$jobStatus', count: {$sum: 1}}}
+    ]);
+
+    stats = stats.reduce((acc, curr) => {
+        const {_id: title, count} = curr;
+        acc[title] = count;
+        return acc;
+
+    }, {});
+
+    console.log(stats);
+
+    const defaultStatus = {
+        pending: stats.pending || 0,
+        interview: stats.interview || 0,
+        rejected: stats.rejected || 0
+    };
+
+    let monthlyApplications = await Job.aggregate([
+        {$match: {createdBy: new mongoose.Types.ObjectId(req.user.userId)}},
+        {
+            $group: {
+                _id: {year: {$year: '$createdAt'}, month: {$month: '$createdAt'}},
+                count: {$sum: 1}
+            }
+        },
+        {$sort: {'_id.year': -1, '_id.month': -1}},
+        {$limit: 6}
+
+    ]);
+
+    monthlyApplications = monthlyApplications.map((item) => {
+        const {_id: {year, month}, count} = item;
+
+        const date = day().month(month - 1).year(year).format('MMM YY');
+
+        return {date, count};
+    }).reverse();
+
+    // let monthlyApplications = [
+    //     {
+    //         date: 'Feb 25',
+    //         count: 23
+    //     },
+    //     {
+    //         date: 'Jan 25',
+    //         count: 37
+    //     },
+    //     {
+    //         date: 'Dec 24',
+    //         count: 32
+    //     }
+    // ]
+
+    res.status(StatusCodes.OK).json({defaultStatus, monthlyApplications})
+
+};
+
 
 
--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {createJob, deleteJob, getAllJobs, getJob, updateJob} from "../controllers/jobController.js";
+import {createJob, deleteJob, getAllJobs, getJob, showStats, updateJob} from "../controllers/jobController.js";
 import {validateIdParam, validateJobInput} from "../middleware/validationMiddleware.js";
 
 
@@ -7,9 +7,14 @@ const router = Router();
 
 router.get('/', getAllJobs);
 router.post('/', validateJobInput, createJob);
+
+
+router.route('/stats').get(showStats);
+
 router.get('/:id', validateIdParam, getJob);
 router.put('/:id', validateJobInput, validateIdParam, updateJob);
 router.delete('/:id', validateIdParam, deleteJob);
+
 
 
 export default router;
